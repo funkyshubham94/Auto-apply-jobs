2cd1da07ecb12e1fc03d0067fa3bac90f94353b1
funkyshubham94 <goyalshubham1117@gmail.com>
2025-08-02 20:45:06 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ['firebasestorage.googleapis.com'],
-  },
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
